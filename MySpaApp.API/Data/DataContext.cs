8f08ddd6a0981106b62e7d619dd2288e953f804f
hkryspin <hkryspin@gmail.com>
2019-02-05 09:19:26 +0100

    Initial Commit

---
+++ MySpaApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using MySpaApp.API.Models;
+
+namespace MySpaApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
